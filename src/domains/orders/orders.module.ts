e473958ac5ba91dc1f28084d8e9030442f642ac7
moses <ghost@moses.com>
2024-04-14 21:09:31 +0100

    import controller

--- src/domains/orders/orders.module.ts
+++ src/domains/orders/orders.module.ts
@@ -1,11 +1,11 @@
 import { PrismaService } from './../../commons/prisma.service';
 import { Module } from '@nestjs/common';
-import { OrderApiController } from './controllers';
+import { OrderApiController, AuthOrderApiController } from './controllers';
 import { OrderApiService } from './services';
 
 @Module({
   imports: [],
-  controllers: [OrderApiController],
+  controllers: [OrderApiController, AuthOrderApiController],
   providers: [OrderApiService, PrismaService],
   exports: [OrderApiService],
 })
